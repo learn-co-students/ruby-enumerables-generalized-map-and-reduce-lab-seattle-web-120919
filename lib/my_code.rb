bbda547b67ecaa81d09b0bb1b4eab759c389428c
Allen Zhang <azhang9328@gmail.com>
2019-12-05 23:43:37 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+def map(array)
+  newarray = []
+  i = 0
+  while i < array.length do
+    newarray.push(yield(array[i]))
+    i += 1
+  end
+  newarray
+end   
+
+def reduce(array, starting_point = nil)
+  i = 0
+  if starting_point
+    total = starting_point
+    else
+    total = array[0]   
+    i = 1
+  end    
+  while i < array.length do
+    total = yield(total, array[i])
+    puts array[i]
+    puts total
+    i += 1
+  end
+  total 
+end   
