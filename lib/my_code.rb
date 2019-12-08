5cbc2fd79d38eda8ca563666eb71d3abfea8b49e
honorable-exception-1170 <aliciachao@gmail.com>
2019-12-08 22:21:56 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+#map methods return a new Array of manipulated elements
+def map(array)
+  new = []
+  i = 0
+  while i < array.length
+      new.push(yield(array[i])) # push manipulated elements into new array
+      i += 1
+  end
+  new
+end
+
+
+def reduce (array, sv=nil)
+  if sv
+    sum = sv
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+
+  while i < array.length
+    sum = yield(sum, array[i]) #pass each element to block
+    i += 1
+  end
+  sum
+end
